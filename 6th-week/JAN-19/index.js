25a29ac5b14b5d3146bad905ef3dd9511c0e585f
Aniket1118 <91197235+Aniket1118@users.noreply.github.com>
2022-01-23 19:57:09 +0530

    some big changes

--- 6th-week/JAN-19/index.js
+++ 6th-week/JAN-19/index.js
@@ -1,54 +1,75 @@
 var reviews = [
-    {
-      id: 1,
-      name: "susan smith",
-      job: "web developer",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {
-      id: 2,
-      name: "anna johnson",
-      job: "web designer",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-      text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
+  {
+    id: 1,
+    name: "Rohit",
+    job: "web developer",
+    img: "https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?b=1&k=20&m=1075599562&s=170667a&w=0&h=cZqbPs4YOWzY6oqR-L5OUXNnC_g-1xl8kxcClNjuk5k=",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray  pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday",
+  },
+  {
+    id: 2,
+    name: "divakar",
+    job: "web designer",
+    img: "https://media.istockphoto.com/photos/website-design-developing-programming-and-coding-technologies-picture-id1197640540?b=1&k=20&m=1197640540&s=170667a&w=0&h=n-7yp4aP8sKZ7CVDAlxn132Pgi7FRjqVHj0ggQMAD8I=",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "dristi",
+    job: "java developer",
+    img: "https://media.istockphoto.com/photos/facial-recognition-system-concept-picture-id872707982?b=1&k=20&m=872707982&s=170667a&w=0&h=rw92Mhgzof0BmxWpW14vZXEOoXKzUQR8MnVkesxRu7w=",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "Dipanshi",
+    job: "software Eng.",
+    img: "https://media.istockphoto.com/photos/    science-technology-concept-scientific-examination-scientist-picture-id1223789403?b=1&k=20&m=1223789403&s=170667a&w=0&h=_x9cZA3kmpNAx-C1PTT1Rqh3PnHrY8x_K2juZ0NI1lw=", 
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually  seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
   ];
   
   var imgElement = document.getElementById("person-img");
-  var author = document.getElementById("author");
   var job = document.getElementById("job");
   var info = document.getElementById("info");
-  var prevBtn = document.querySelector(".prev-btn");
-  var nextBtn = document.querySelector(".next-btn");
+  var prv = document.querySelector(".prv");
+  var next = document.querySelector(".next");
   var randomBtn = document.querySelector(".random-btn");
-  
-  //reviews = [ {}, {} , {}]
-  
-  var currentIndex = 2;
-  
-  prevBtn.addEventListener("click", prevClick);
-  nextBtn.addEventListener("click", nextClick);
+
+  var currentIndex = 0;
+  prv.addEventListener("click", prev);
+  next.addEventListener("click", nexton);
   randomBtn.addEventListener("click", randomaize);
-  
-  updateUserDetails();
-  function updateUserDetails() {
-    imgElement.src = reviews[currentIndex].img;
-    author.innerText = reviews[currentIndex].name;
-    job.innerText = reviews[currentIndex].job;
-    info.innerText = reviews[currentIndex].text;
+
+
+  function prev() {
+    if (currentIndex === 0) {
+      currentIndex = reviews.length - 1;
+    } else {
+      currentIndex--;
+    }
+    console.log(currentIndex);
+    update();
   }
+  function nexton() {
+    if (currentIndex === reviews.length - 1) {
+      currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+    console.log(currentIndex);
+    update();
+  }
+  function randomaize() {
+    currentIndex = Math.floor(Math.random() * reviews.length);
+    console.log(currentIndex);
+    update();
+  }
+  function update() { 
+     imgElement.src = reviews[currentIndex].img;
+      uname.innerText = reviews[currentIndex].name;
+      job.innerText = reviews[currentIndex].job;
+      info.innerText = reviews[currentIndex].text;
+
+  }
+  
